d1752880add4909fc17808f9d6c537d2c3d83994
Shogo Takata <pineapplehunter.daniel@gmail.com>
2021-10-27 17:51:23 +0900

    update repr

--- tuat_feed/post.py
+++ tuat_feed/post.py
@@ -1,37 +1,37 @@
 from __future__ import annotations
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from datetime import datetime, date
 from typing import Dict, List
 
 
-@dataclass(repr=True, eq=True)
+@dataclass(repr=True, eq=True, frozen=True)
 class Attachment:
     """添付ファイルデータ"""
 
     name: str  # ファイル名
-    url: str  # URL
+    url: str = field(repr=False)  # URL
 
 
-@dataclass(repr=True)
+@dataclass(repr=True, eq=True, frozen=True)
 class Post:
     """
     掲示板の投稿
     """
 
-    post_id: int  # 投稿ID（内部処理用）
+    post_id: int = field(repr=False)  # 投稿ID（内部処理用）
     title: str  # タイトル
-    description: str  # 本文
-    update_date: date  # 最終更新日
-    show_date_start: date  # 公開期間（開始）
-    show_date_end: date  # 公開期間（終了）
-    author: str  # 担当者
-    origin: str  # 発信元
-    category: str  # カテゴリー
-    attachment: List[Attachment]  # 添付ファイル
-    other: Dict[str, str]  # その他のフィールド
+    description: str = field(repr=False)  # 本文
+    update_date: date = field(repr=False)  # 最終更新日
+    show_date_start: date = field(repr=False)  # 公開期間（開始）
+    show_date_end: date = field(repr=False)  # 公開期間（終了）
+    author: str = field(repr=False)  # 担当者
+    origin: str = field(repr=False)  # 発信元
+    category: str = field(repr=False)  # カテゴリー
+    attachment: List[Attachment] = field(repr=False)  # 添付ファイル
+    other: Dict[str, str] = field(repr=False)  # その他のフィールド
 
-    @classmethod
-    def parse_post(cls, post_raw: dict) -> Post:
+    @staticmethod
+    def parse_post(post_raw: dict) -> Post:
         """取得された生データから投稿情報のクラスに変換します
 
         Parameters
