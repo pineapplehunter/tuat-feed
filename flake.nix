b8689849de13dcc65ce336aef9c25e1f2bd5be6e
Shogo Takata <peshogo@gmail.com>
2023-10-24 11:43:33 +0900

    update deps and add flake

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "A very basic flake";
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = with pkgs;[
+          poetry
+          python3
+        ];
+      };
+    };
+}
